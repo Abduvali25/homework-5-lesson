456d8abb98b888b485b73d8b084f8fa06ef296ab
Abduvali <itabduvali@gamil.com>
2024-07-29 01:28:45 +0500

    chended

--- app/products/[id]/page.js
+++ app/products/[id]/page.js
@@ -5,8 +5,11 @@ export default async function page({params}) {
     const product = await response.json();
   return (
     <div>
-        <h1>Product id:{params.id}</h1>
-        <h2>{product && product.title}</h2>
+        <h1   className='mb-3'>Product id:{params.id}</h1>
+        <h2 > Name: {product && product.title}</h2>
+        <h2> Description: {product && product.description}</h2>
+        <h2> Price: {product && product.price}</h2>
+        <h2>{product && product.image}</h2>
     </div>
   )
 }
